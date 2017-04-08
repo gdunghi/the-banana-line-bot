b8493890f4fc9eaf211e907f77534467ee3d2881
jirayu <jirayu.photisan@gmail.com>
2017-04-09 02:21:35 +0700

    add fix request next lottery

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const router = Express.Router()
 
 export default [
   router.get('/ping', (req, res) => { res.status(200).json({ success: true }) }),
+
   router.post('/webhook', (req, res, next) => {
     const body = req.body
     if (_.isEmpty(body)) {
@@ -16,41 +17,62 @@ export default [
     }
 
     const events = body.events
-    console.info(events)
-    const reply = {
-      replyToken: events[0].replyToken,
-      messages: [
-        {
-            "type":"text",
-            "text":"Hello, user"
-        },
-        {
-            "type":"text",
-            "text":"May I help you?"
-        }
-      ]
-    }
-
-    axios.post(
-      'https://api.line.me/v2/bot/message/reply',
-      reply,
+    axios.post('http://127.0.0.1:5000/lottery/get',
+      { "day": 16, "month": 4, "year": 2560 },
       {
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+          'Content-Type': 'application/json'
         }
-      }
-    ).then((response) => {
-      res.status(200).json({ success: true })
-    })
-    .catch((err) => {
-      const response = err.response
-      const statusCode = response.status
-      if (statusCode === 400) {
-        next(Boom.badRequest(JSON.stringify(response.data)))
+      }).then((result) => {
+        let reply = {
+          replyToken: events[0].replyToken,
+          messages: [
+            {
+              "type": "text",
+              "text": "หวยงวดหน้าคือ"
+            },
+            {
+              "type": "text",
+              "text": JSON.stringify(result.data)
+            }
+          ]
+        }
+        return reply
+      })
+      .then((reply) => {
+        axios.post(
+          'https://api.line.me/v2/bot/message/reply',
+          reply,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+            }
+          }
+        ).then((response) => {
+          res.status(200).json({ success: true })
+        })
+          .catch((err) => {
+            const response = err.response
+            const statusCode = response.status
+            if (statusCode === 400) {
+              next(Boom.badRequest(JSON.stringify(response.data)))
+              return
+            }
+            next(Boom.notFound(JSON.stringify(response.data)))
+          })
+      })
+      .catch((error) => {
+        const response = error.response
+        const statusCode = response.status
+        if (statusCode === 400) {
+          next(Boom.badRequest(JSON.stringify(response.data)))
+          return
+        }
+        next(Boom.notFound(JSON.stringify(response.data)))
         return
-      }
-      next(Boom.notFound(JSON.stringify(response.data)))
-    })
+      })
+
+
   })
 ]
