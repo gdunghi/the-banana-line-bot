501d849b843c9a0f8f1069453b7406e431963e8f
jirayu <jirayu.photisan@gmail.com>
2017-04-09 16:20:33 +0700

    log

--- src/App.js
+++ src/App.js
@@ -16,67 +16,87 @@ export default [
       return
     }
 
-   
+
 
     const events = body.events
-    console.info(events)
-
-    axios.post(process.env.LOTTERY_API,
-      { "day": 16, "month": 4, "year": 2560 },
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then((result) => {
-        let reply = {
-          replyToken: events[0].replyToken,
-          messages: [
-            {
-              "type": "text",
-              "text": "หวยงวดหน้าคือ"
-            },
-            {
-              "type": "text",
-              "text": JSON.stringify(result.data)
-            }
-          ]
-        }
-        return reply
-      })
-      .then((reply) => {
-        axios.post(
-          'https://api.line.me/v2/bot/message/reply',
-          reply,
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
-            }
+    if ('00000000000000000000000000000000' === events[0].replyToken) {
+      axios.post(
+        'https://api.line.me/v2/bot/message/reply',
+        reply,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
           }
-        ).then((response) => {
-          res.status(200).json({ success: true })
-        })
-          .catch((err) => {
-            const response = err.response
-            const statusCode = response.status
-            if (statusCode === 400) {
-              next(Boom.badRequest(JSON.stringify(response.data)))
-              return
-            }
-            next(Boom.notFound(JSON.stringify(response.data)))
-          })
-      })
-      .catch((error) => {
-        const response = error.response
-        const statusCode = response.status
-        if (statusCode === 400) {
-          next(Boom.badRequest(JSON.stringify(response.data)))
-          return
         }
-        next(Boom.notFound(JSON.stringify(response.data)))
-        return
+      ).then((response) => {
+        res.status(200).json({ success: true })
       })
-
-
+        .catch((err) => {
+          const response = err.response
+          const statusCode = response.status
+          if (statusCode === 400) {
+            next(Boom.badRequest(JSON.stringify(response.data)))
+            return
+          }
+          next(Boom.notFound(JSON.stringify(response.data)))
+        })
+    } else {
+      axios.post(process.env.LOTTERY_API,
+        { "day": 16, "month": 4, "year": 2560 },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then((result) => {
+          let reply = {
+            replyToken: events[0].replyToken,
+            messages: [
+              {
+                "type": "text",
+                "text": "หวยงวดหน้าคือ"
+              },
+              {
+                "type": "text",
+                "text": JSON.stringify(result.data)
+              }
+            ]
+          }
+          return reply
+        })
+        .then((reply) => {
+          axios.post(
+            'https://api.line.me/v2/bot/message/reply',
+            reply,
+            {
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+              }
+            }
+          ).then((response) => {
+            res.status(200).json({ success: true })
+          })
+            .catch((err) => {
+              const response = err.response
+              const statusCode = response.status
+              if (statusCode === 400) {
+                next(Boom.badRequest(JSON.stringify(response.data)))
+                return
+              }
+              next(Boom.notFound(JSON.stringify(response.data)))
+            })
+        })
+        .catch((error) => {
+          const response = error.response
+          const statusCode = response.status
+          if (statusCode === 400) {
+            next(Boom.badRequest(JSON.stringify(response.data)))
+            return
+          }
+          next(Boom.notFound(JSON.stringify(response.data)))
+          return
+        })
+    }
   })
 ]
